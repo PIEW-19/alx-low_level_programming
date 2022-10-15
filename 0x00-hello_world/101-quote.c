dcebe073387ae6cba8c518d6bf843670e93a82e3
PIEW-19 <precious.nwanya@gmail.com>
2022-10-15 05:54:10 -0700

    Using function called write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - the entry point of programme
+ * Return: return 1 if no error
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
